537da312dc994a60b53a9d12ad3fc52aabaf137b
mm <maciej.markiewicz10@gmail.com>
2021-06-06 09:55:32 +0200

    clion

--- lab11.cpp
+++ lab11.cpp
@@ -32,20 +32,6 @@ void wyswietlTablice(const short T[], int n) {
     cout << endl << "Suma=" << suma << endl << endl;
 }
 
-void sumujOdDo(const short T[], int poczatek, int koniec, short &wynik) {
-
-    string k = Kolor::nastepny(), r = Kolor::reset();
-
-    wynik = 0;
-    for (int i = poczatek; i < koniec; i++) {
-        wynik += T[i];
-
-        string info = k + to_string(T[i]) + r + "\n";
-        cout << info;
-    }
-
-}
-
 
 void l3z1() {
     static short *TAB; //Tablica
@@ -54,7 +40,7 @@ void l3z1() {
     wypelnijLosowymi(TAB, rozmiarTablicy);
 
     wyswietlTablice(TAB, rozmiarTablicy);
-    unsigned int polowa = rozmiarTablicy /2;
+    unsigned int polowa = rozmiarTablicy / 2;
     omp_set_num_threads(2);
     string k = Kolor::nastepny(), r = Kolor::reset();
     int wynik = 0;
@@ -62,37 +48,23 @@ void l3z1() {
     {
 
         int wynikW = 0;
-#pragma omp for schedule(dynamic,polowa)
+#pragma omp for schedule(dynamic, polowa)
         for (int i = 0; i < rozmiarTablicy; i++) {
             wynikW += TAB[i];
 
             string info = k + to_string(TAB[i]) + r + "\n";
             cout << info;
-    };
-        cout << "suma watku "+ std::to_string(wynikW)+"\n";
-        wynik+=wynikW;
+        };
+        cout << "suma watku= " + std::to_string(wynikW) + "\n";
+        wynik += wynikW;
     }
 
-    cout << "suma  "+ std::to_string(wynik)+"\n";
+    cout << "suma=  " + std::to_string(wynik) + "\n";
 
 
     delete[] TAB;
 }
 
-
-void sumujCoDrugi(const short T[], int poczatek, int koniec, short &wynik) {
-
-    string k = Kolor::nastepny(), r = Kolor::reset();
-
-    wynik = 0;
-    for (int i = poczatek; i < koniec; i = i + 2) {
-        wynik += T[i];
-
-        string info = k + to_string(T[i]) + r + "\n";
-        cout << info;
-    }
-
-}
 
 void l3z2() {
     static short *TAB; //Tablica
@@ -102,40 +74,30 @@ void l3z2() {
 
     wyswietlTablice(TAB, rozmiarTablicy);
 
-    thread watek1(sumujCoDrugi, TAB, 0, rozmiarTablicy, ref(wynik1));
-    thread watek2(sumujCoDrugi, TAB, 1, rozmiarTablicy, ref(wynik2));
+    omp_set_num_threads(2);
+    string k = Kolor::nastepny(), r = Kolor::reset();
+    int wynik = 0;
+#pragma omp parallel
+    {
 
-    watek1.join();
-    watek2.join();
+        int wynikW = 0;
+#pragma omp for schedule(static, 1)
+        for (int i = 0; i < rozmiarTablicy; i++) {
+            wynikW += TAB[i];
 
+            string info = k + to_string(TAB[i]) + r + "\n";
+            cout << info;
+        };
+        cout << "suma watku= " + std::to_string(wynikW) + "\n";
+        wynik += wynikW;
+    }
 
-    cout << endl << "Wyniki pracy wątków" << endl
-         << "suma1=" << wynik1 << " suma2=" << wynik2
-         << " suma=" << wynik1 + wynik2 << endl << endl;
+    cout << "suma=  " + std::to_string(wynik) + "\n";
+
 
     delete[] TAB;
 }
 
-void sumujZnak(const short T[], char znak, int koniec, short &wynik) {
-
-    string k = Kolor::nastepny(), r = Kolor::reset();
-
-    wynik = 0;
-    for (int i = 0; i < koniec; i++) {
-        if (znak == '-' && T[i] < 0) {
-            wynik += T[i];
-            string info = k + to_string(T[i]) + r + "\n";
-            cout << info;
-        } else if (znak == '+' && T[i] > 0) {
-            wynik += T[i];
-            string info = k + to_string(T[i]) + r + "\n";
-            cout << info;
-        }
-
-
-    }
-
-}
 
 void l3z3() {
     static short *TAB; //Tablica
@@ -145,16 +107,42 @@ void l3z3() {
 
     wyswietlTablice(TAB, rozmiarTablicy);
 
-    thread watek1(sumujZnak, TAB, '-', rozmiarTablicy, ref(wynik1));
-    thread watek2(sumujZnak, TAB, '+', rozmiarTablicy, ref(wynik2));
+    omp_set_num_threads(2);
+    string k = Kolor::nastepny(), r = Kolor::reset();
+    int wynik = 0;
+    bool isMinusTaken = false;
+    omp_set_num_threads(2);
+#pragma omp parallel
+    {
+        bool addMinus = false;
+        if (isMinusTaken == false) {
+            isMinusTaken = true;
+            addMinus = true;
+        }
+        int wynikW = 0;
 
-    watek1.join();
-    watek2.join();
+        for (int i = 0; i < rozmiarTablicy; i++) {
+            if (addMinus && TAB[i] < 0) {
+                wynikW += TAB[i];
+
+                string info = k + to_string(TAB[i]) + r + "\n";
+                cout << info;
+            }
+            if (!addMinus && TAB[i] > 0) {
+                wynikW += TAB[i];
+
+                string info = k + to_string(TAB[i]) + r + "\n";
+                cout << info;
+            }
 
 
-    cout << endl << "Wyniki pracy wątków" << endl
-         << "suma1=" << wynik1 << " suma2=" << wynik2
-         << " suma=" << wynik1 + wynik2 << endl << endl;
+        };
+        cout << "suma watku= " + std::to_string(wynikW) + "\n";
+        wynik += wynikW;
+    }
+
+    cout << "suma=  " + std::to_string(wynik) + "\n";
+
 
     delete[] TAB;
 }
@@ -183,11 +171,26 @@ void l3z4() {
 
     wyswietlTablice(TAB, rozmiarTablicy);
 
-    thread watek1(kwadratujIPodminCoDrugi, TAB, 0, rozmiarTablicy);
-    thread watek2(kwadratujIPodminCoDrugi, TAB, 1, rozmiarTablicy);
 
-    watek1.join();
-    watek2.join();
+    omp_set_num_threads(2);
+    string k = Kolor::nastepny(), r = Kolor::reset();
+    int wynik = 0;
+
+    {
+
+        ;
+#pragma omp for
+        for (int i = 0; i < rozmiarTablicy; i++) {
+            string info = k + to_string(TAB[i]) + r + "\n";
+            cout << info;
+
+            TAB[i] = TAB[i] * TAB[i];
+        };
+
+    }
+
+
+    ;
 
     wyswietlTablice(TAB, rozmiarTablicy);
 
@@ -239,11 +242,29 @@ void l3z5() {
     cout << "Dane" << endl;
     wyswietlTablice(TAB, rozmiarTablicy);
 
-    thread watek1(filtrDyskretnyCoDrugi, TAB, resTAB, 0, rozmiarTablicy);
-    thread watek2(filtrDyskretnyCoDrugi, TAB, resTAB, 1, rozmiarTablicy);
 
-    watek1.join();
-    watek2.join();
+    string k = Kolor::nastepny(), r = Kolor::reset();
+
+
+    omp_set_num_threads(2);
+#pragma omp for
+    for (int i = 0; i < rozmiarTablicy; i++) {
+
+
+        string info = k + to_string(TAB[i]) + r + "\n";
+        cout << info;
+
+        if (i == 0) {
+            resTAB[i] = (float) (TAB[i] + TAB[i + 1]) / 2;
+        } else if (i == rozmiarTablicy - 1) {
+            resTAB[i] = (float) (TAB[i - 1] + TAB[i]) / 2;
+        } else {
+            resTAB[i] = (float) (TAB[i - 1] + TAB[i] + TAB[i + 1]) / 3;
+        }
+
+
+    }
+
 
     cout << "Wyniki" << endl;
     wyswietlTablice(resTAB, rozmiarTablicy);
--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ int main() {
 
     //lab3
 
-  std::cout<<"l3z1"<<std::endl;
+  std::cout<<"l11z1"<<std::endl;
  l3z1();
-  std::cout<<"l3z2"<<std::endl;
-// l3z2();
-  std::cout<<"l3z3"<<std::endl;
-// l3z3();
-  std::cout<<"l3z4"<<std::endl;
-// l3z4();
-  std::cout<<"l3z5"<<std::endl;
-// l3z5();
+  std::cout<<"l11z2"<<std::endl;
+ l3z2();
+  std::cout<<"l11z3"<<std::endl;
+ l3z3();
+  std::cout<<"l11z4"<<std::endl;
+ l3z4();
+  std::cout<<"l11z5"<<std::endl;
+ l3z5();
 
   return 0;
 }
